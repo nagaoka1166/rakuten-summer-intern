17425b3494b4c6314be4f8173b8cfa2374f4ff10
nagaoka1166 <ryuu11660408@gmail.com>
2022-08-30 10:45:12 +0900

    ヘッダーの大枠作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,16 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import SettingsSharpIcon from '@mui/icons-material/SettingsSharp';
+
 
 const Header = () => <Box sx={{ flexGrow: 1 }}>
   <AppBar position="static">
     <Toolbar>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Rakuten トマレル
+      </Typography>
       <IconButton
         size="large"
         edge="start"
@@ -17,12 +20,8 @@ const Header = () => <Box sx={{ flexGrow: 1 }}>
         aria-label="menu"
         sx={{ mr: 2 }}
       >
-        <MenuIcon />
+        <SettingsSharpIcon />
       </IconButton>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        News
-      </Typography>
-      <Button color="inherit">Login</Button>
     </Toolbar>
   </AppBar>
 </Box>
