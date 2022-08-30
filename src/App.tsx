ed619ae9811d372879dc87321c08e4dce3cb19e2
kyuki3rain <kyuki3rain>
2022-08-30 15:51:47 +0900

    headerが上に張り付くように修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
@@ -7,10 +9,14 @@ import SearchResultPage from './pages/SearchResultPage';
 const App = () => {
   const [editOption, setEditOption] = useState(false);
 
-  return <div>
-    <Header setEditOption={setEditOption}/>
-    {editOption ? <EditOptionPage /> : <SearchResultPage />}
-  </div>
+  return (
+    <Container maxWidth="sm" style={{ padding: 0, marginTop: -32 }}>
+      <Box sx={{ my: 4 }}>
+        <Header setEditOption={setEditOption} />
+        {editOption ? <EditOptionPage /> : <SearchResultPage />}
+      </Box>
+    </Container>
+  );
 };
 
 export default App;
