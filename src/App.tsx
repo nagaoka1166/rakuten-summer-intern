44d1f10b51c543a58caefe9362f7c6efedd7ea35
kyuki3rain <kyuki3rain>
2022-08-31 11:43:08 +0900

    色々調整

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
 import './App.css';
-import CircularProgress from '@mui/material/CircularProgress';
 import Header from './components/Header';
 import EditOptionPage from './pages/EditOptionPage';
 import SearchResultPage from './pages/SearchResultPage';
-import { useData } from './hooks/useData';
 
 const App = () => {
   const [editOption, setEditOption] = useState(false);
@@ -15,7 +13,7 @@ const App = () => {
     <Container maxWidth="sm" style={{ padding: 0, marginTop: -32 }}>
       <Box sx={{ my: 4 }}>
         <Header setEditOption={setEditOption} />
-        {editOption ? <EditOptionPage setEditOption={setEditOption} /> : <SearchResultPage editOption={editOption} />}
+        {editOption ? <EditOptionPage setEditOption={setEditOption} /> : <SearchResultPage />}
       </Box>
     </Container>
   );
